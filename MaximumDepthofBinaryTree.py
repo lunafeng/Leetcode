9a4ff0a49ed64ecd6f4feff605986cc0db033dfc
Luna Feng <LunasMacbook@Lunas-MacBook-Pro.local>
2015-12-15 14:09:42 -0500

    Get maximum depth of a binary tree

---
+++ MaximumDepthofBinaryTree.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+
+class Solution(object):
+	def maxDepth(self,root):
+		if root is None:
+			return 0
+		else:
+			return max(self.maxDepth(root.left), self.maxDepth(root.right)) + 1	
+			
